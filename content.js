2a1084432b86a1160b8f0fbc20f530d27730158f
violet edwards <113566839+violaflora@users.noreply.github.com>
2022-12-16 03:35:52 -0500

    Update content.js

--- content.js
+++ content.js
@@ -6,6 +6,7 @@ const observer = new MutationObserver(mutations => {
     
     document.querySelectorAll("._3zucw")[0].remove(); /*toolbar ad*/
     document.querySelectorAll(".Ekspr")[0].remove(); /*top sidebar ad*/
+    document.querySelectorAll("._1T8FO")[0].remove(); /*shop ad*/
     document.querySelectorAll("._1eTnJ")[0].remove(); /*review icon*/
     
     document.querySelectorAll("._22cC8")[0].src = "https://cdn.discordapp.com/attachments/936701108099907594/1051712588930621530/70a4be81077a8037698067f583816ff9.png"; /*logo replacement*/
